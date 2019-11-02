ef881151207d66a6d0c29987a47ab17b56463b85
Tobias Seidel <tobiasseidel@mail.de>
2019-11-02 23:26:54 +0100

    Finished basic functionality (quick and dirty)

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import requests
 import numpy as np
@@ -5,12 +6,46 @@ import re
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+import datetime
+import pandas_datareader.data as web
+from PIL import Image
 
 data = []
-symbol = 'AAPL'
+symbol_morn = 'ABT'
+symbol_yhoo = 'ABT'
 
-url1 = 'http://financials.morningstar.com/finan/financials/getFinancePart.html?&callback=xxx&t={}'.format(symbol)
-url2 = 'http://financials.morningstar.com/finan/financials/getKeyStatPart.html?&callback=xxx&t={}'.format(symbol)
+
+print("Enter US Stock Symbol or type 'n':")
+s = input()
+if s == "n":
+    print("Enter Morningstar-Symbol:")
+    symbol_morn = input()
+    print("Enter Yahoo-Symbol:")
+    symbol_yhoo = input()
+else:
+    symbol_morn = s
+    symbol_yhoo = symbol_morn
+
+df_est = pd.read_html(r'http://financials.morningstar.com/valuate/annual-estimate-list.action?&t={}'.format(symbol_morn),keep_default_na=False)
+df_est = pd.concat(df_est)
+df3 = pd.DataFrame([[df_est[1][4],df_est[1][6]],[df_est[4][4],df_est[4][6]]],index=[df_est[2][0],df_est[5][0]],columns=["Median EPS","Mean EPS"])
+df3.index = pd.to_datetime(df3.index)
+df3 = df3.apply(pd.to_numeric, errors='coerce')
+est_time = df_est[2][0] + "-" + df_est[5][0]
+df_est[0][2] = est_time
+df_est[1][2] = df_est[1][2] + " " + df_est[2][0]
+df_est[2][2] = df_est[2][2] + " " + df_est[2][0]
+df_est[4][2] = df_est[4][2] + " " + df_est[5][0]
+df_est[5][2] = df_est[5][2] + " " + df_est[5][0]
+df_est[0].replace('', np.nan, inplace=True)
+df_est.dropna(axis=0, how='any', inplace=True)
+header = df_est.iloc[0]
+df_est = df_est[1:]
+df_est.rename(columns = header, inplace=True)
+df_est.set_index(est_time, inplace=True)
+
+url1 = 'http://financials.morningstar.com/finan/financials/getFinancePart.html?&callback=xxx&t={}'.format(symbol_morn)
+url2 = 'http://financials.morningstar.com/finan/financials/getKeyStatPart.html?&callback=xxx&t={}'.format(symbol_morn)
 
 soup1 = BeautifulSoup(json.loads(re.findall(r'xxx\((.*)\)', requests.get(url1).text)[0])['componentData'], 'lxml')
 soup2 = BeautifulSoup(json.loads(re.findall(r'xxx\((.*)\)', requests.get(url2).text)[0])['componentData'], 'lxml')
@@ -40,16 +75,59 @@ column_name=[]
 for col in df.columns:
     column_name.append(col)
 
-df[column_name[5]] = df[column_name[5]].apply(lambda x: x*15)
-df[column_name[6]] = df[column_name[6]].apply(lambda x: x*15)
+start = df.index[0]
+end = datetime.date.today()
+df_price = web.DataReader(symbol_yhoo,'yahoo',start,end)
+test = np.append(df.index.values,df3.index.values)
+test2 = np.append(df[column_name[5]].values,df3["Median EPS"].values)
+currency = str(df.columns[13])[-3:]
+xlabel = []
 
-file = "AAPL Historical Prices.csv"
-df2 = pd.read_csv(file, delimiter=',')
-df2['Date'] = pd.to_datetime(df2['Date'])
+multiple = (df3["Median EPS"][1]/df[column_name[5]][0]-1.0)
+print("Choose Multiple(base/peg/peg85/pegc):")
+s = input
+if s == "base":
+    multiple = 15.0
+elif s == "peg":
+    if multiple < 15.0:
+        multiple = 15.0
+elif s == "peg85":
+    multiple = multiple + 8.5
+elif s == "pegc":
+    for x range(1, len(test)):
+        gnumbers = ((population[pop] - population[pop-1]) / population[pop-1] * 100)
+        print(gnumbers)
 
-f1, ax = plt.subplots(figsize = (10,5))
-plt.fill_between(df.index, df[column_name[5]], color = "green")
-plt.plot(df.index, df[column_name[5]], color="orange", linewidth=3)
-plt.plot(df.index, df[column_name[6]], color="white")
-plt.plot(df2["Date"], df2["Close"], color="black")
-plt.show()
+df[column_name[5]] = df[column_name[5]].apply(lambda x: x*multiple)
+df3["Median EPS"] = df3["Median EPS"]*multiple
+df[column_name[6]] = df[column_name[6]].apply(lambda x: x*multiple)
+
+for x in df.index:
+    if not pd.isnull(x):
+        xlabel.append(x.strftime('%m/%y'))
+for x in df3.index:
+    if not pd.isnull(x):
+        xlabel.append(x.strftime('%m/%y'))
+plt.style.use('dark_background')
+f1, ax = plt.subplots(figsize = (8,5))
+ax.set_title(symbol_yhoo)
+ax.set_ylabel(currency)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+#ax.spines["bottom"].set_visible(False)
+#ax.spines["left"].set_visible(False)
+#ax.get_xaxis().tick_bottom()
+#ax.get_yaxis().tick_left()
+ax.set_axisbelow(True)
+#ax.yaxis.grid(color='gray', linewidth=0.25)
+ax.xaxis.grid(color='gray', linewidth=0.25, alpha=0.75)
+plt.xticks(test,xlabel)
+plt.fill_between(test, test2, color = "blue")
+plt.plot(test, test2, color="grey", linewidth=3, marker="^")
+plt.plot(df.index, df[column_name[6]], color="yellow", marker="o")
+plt.plot(df_price.index, df_price["Close"], color="white")
+plt.ylim(0,None)
+plt.xlim(df.index[0],None)
+plt.savefig('Graphs/plot.png')
+image = Image.open(r'Graphs/plot.png')
+image.show()
